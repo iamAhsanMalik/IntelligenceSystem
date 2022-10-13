4eb49f99ef0b232a2bf1f92376a85aec60cfb174
Ahsan Joiya <ahsan.joiya@appinsnap.com>
2022-10-13 15:28:06 +0500

    Added: Identity configurations for ApplicationUser and apply migrations

---
+++ src/IFC/Infrastructure/Identity/Services/AuthService.cs
@@ -0,0 +1,41 @@
+using IFC.Application.DTOs;
+using MapsterMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+
+namespace IFC.Infrastructure.Identity.Services;
+internal class AuthService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IMapper _mapper;
+
+    public AuthService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IMapper mapper)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _mapper = mapper;
+    }
+
+    #region Login
+    public async Task LoginAsync(ApplicationUser user, bool isPersistence = false) => await _signInManager.SignInAsync(user, isPersistent: isPersistence);
+    public async Task<Microsoft.AspNetCore.Identity.SignInResult?> PasswordLoginAsync(LoginDTO model, bool lockoutUserOnFailure = false) =>
+        !string.IsNullOrEmpty(model.Email) && !string.IsNullOrEmpty(model.Password) ? await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: lockoutUserOnFailure) : default;
+
+    #endregion
+
+    #region Register
+    public async Task<IdentityResult?> RegisterAsync(ApplicationUser user) => await _userManager.CreateAsync(user);
+    public async Task<IdentityResult?> RegisterWithPasswordAsync(RegisterDTO model) => await _userManager.CreateAsync(_mapper.Map<ApplicationUser>(model), model.Password);
+
+    #endregion
+
+    #region Logout
+    public async Task LogoutAsync() => await _signInManager.SignOutAsync();
+    #endregion
+
+    #region Password Reset Token Generator
+    public async Task<string> PasswordResetTokenGeneratorAsync(ApplicationUser user) => WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(await _userManager.GeneratePasswordResetTokenAsync(user)));
+    #endregion
+}
