8a93f9223a24beca656159857805ad4d51a6ef90
Ahsan Joyia <ahsan.joiya@appinsnap.com>
2022-10-27 12:52:51 +0500

    Created mapping profile for different profiles

---
+++ src/IFC/Application/Profiles/SuspectProfileProfile.cs
@@ -0,0 +1,17 @@
+﻿using IFC.Application.DTOs.Threat;
+using Mapster;
+
+namespace IFC.Application.Profiles;
+internal class SuspectProfileProfile : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        TypeAdapterConfig<SuspectProfile, ThreatDTO>
+        .NewConfig()
+        .Map(dest => dest.Organization, src => $"{src.Organization!.Name}")
+        .Map(dest => dest.Wing, src => $"{src.Wing!.Name}")
+        .Map(dest => dest.SuspectsProfile, src => $"{src.SuspectsProfile!.FullName}")
+        .Map(dest => dest.Incident, src => $"{src.Incident!.IncidentDate}")
+        .Map(dest => dest.Location, src => $"{src.Location!.Name}");
+    }
+}
