a204f0a34fe373eded383b85200cd9b5bba174f2
Ahsan Joyia <ahsan.joiya@appinsnap.com>
2022-10-28 12:06:22 +0500

    Updated repos to return DTO instead of Entity

--- src/IFC/Application/Contracts/Persistence/Repositries/IAddressRepo.cs
+++ src/IFC/Application/Contracts/Persistence/Repositries/IAddressRepo.cs
@@ -1,9 +1,11 @@
-﻿namespace IFC.Application.Contracts.Persistence.Repositries;
+﻿using IFC.Application.DTOs.Address;
+
+namespace IFC.Application.Contracts.Persistence.Repositries;
 
 public interface IAddressRepo
 {
     Task CreateAddressAsync(Address address);
     Task DeleteAddressAsync(long? id);
-    Task<List<Address>> GetAddressesAsync();
-    Task<Address?> GetAddressesAsync(long? id);
+    Task<List<AddressDTO>> GetAddressesAsync();
+    Task<AddressDTO> GetAddressesAsync(long? id);
 }
