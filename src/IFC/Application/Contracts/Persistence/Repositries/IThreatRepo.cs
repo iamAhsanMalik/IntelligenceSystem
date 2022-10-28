7d0c9983f01a1d2d40f19d35365aa6711a8aa313
Rohban haider <rohban.haider@appinsnap.com>
2022-10-28 14:42:56 +0500

    Change in Repos

--- src/IFC/Application/Contracts/Persistence/Repositries/IThreatRepo.cs
+++ src/IFC/Application/Contracts/Persistence/Repositries/IThreatRepo.cs
@@ -1,9 +1,11 @@
-﻿namespace IFC.Application.Contracts.Persistence.Repositries;
+﻿using IFC.Application.DTOs.Threat;
+
+namespace IFC.Application.Contracts.Persistence.Repositries;
 
 public interface IThreatRepo
 {
     Task CreateThreatsAsync(Threat threat);
     Task DeleteThreatsAsync(long? id);
-    Task<List<Threat>> GetThreatsAsync();
-    Task<Threat?> GetThreatsAsync(long? id);
+    Task<List<ThreatDTO>> GetThreatsAsync();
+    Task<ThreatDTO> GetThreatsAsync(long? id);
 }
