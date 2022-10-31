bcb0a64c425f8530100524573864c06b9311455b
Ahsan Joyia <ahsan.joiya@appinsnap.com>
2022-10-31 12:28:05 +0500

    Integrated charts and deleted unecassary scripts and cs files

--- src/IFC/DataTableServices/HtmlService/IHtmlService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace IFC.DataTableServices.HtmlService;
-
-public interface IHtmlService
-{
-    byte[] Write<T>(IList<T> data);
-}
--- src/IFC/DependencyInjection.cs
+++ src/IFC/DependencyInjection.cs
@@ -1,7 +1,3 @@
-using IFC.DataTableServices.CsvService;
-using IFC.DataTableServices.ExcelService;
-using IFC.DataTableServices.ExportService;
-using IFC.DataTableServices.HtmlService;
 using Newtonsoft.Json.Converters;
 
 namespace IFC;
@@ -11,10 +7,6 @@ public static class DependencyInjection
     public static IServiceCollection AddIFCServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddApplicationServices().AddInfrastructureServices(configuration);
-        services.AddScoped<IExportService, ExportService>();
-        services.AddScoped<IExcelService, ExcelService>();
-        services.AddScoped<ICsvService, CsvService>();
-        services.AddScoped<IHtmlService, HtmlService>();
         var authorizeFilter = new AuthorizeFilter(new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build());
         services.AddControllersWithViews(options => options.Filters.Add(authorizeFilter)).AddNewtonsoftJson(options => options.SerializerSettings.Converters.Add(new StringEnumConverter())).AddJsonOptions(options =>
                 options.JsonSerializerOptions.PropertyNamingPolicy = null);
