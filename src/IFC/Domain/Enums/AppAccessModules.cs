e3c3277bddbc85d9a4318020e9281e9b5b40493f
Ahsan Joyia <ahsan.joiya@appinsnap.com>
2022-11-02 07:32:54 +0500

    Added Permission module enums and change the threat level file name to match naming conventions

---
+++ src/IFC/Domain/Enums/AppAccessModules.cs
@@ -0,0 +1,29 @@
+﻿namespace IFC.Domain.Enums;
+/// <summary>
+/// Declare IFC modulue name here to generate permissions for each module
+/// </summary>
+public enum AppAccessModules
+{
+    Address,
+    Affiliate,
+    Approval,
+    ApprovalRequestType,
+    City,
+    District,
+    Funder,
+    Incident,
+    Involments,
+    Location,
+    OperationalBase,
+    Organization,
+    OrganizationFunder,
+    RelationType,
+    SectorHeadQuarter,
+    SocialMediaProfile,
+    SuspectFamilyDetail,
+    TerrioristFamilyDetail,
+    TerroristInvolvement,
+    TerroristProfile,
+    Threat,
+    Wing
+}
--- src/IFC/Domain/Enums/threatlevel.cs
+++ src/IFC/Domain/Enums/threatlevel.cs
@@ -1,9 +1,8 @@
 ﻿namespace IFC.Domain.Enums;
 public enum Threatlevel
 {
-    High=1,
-    Mediume=2,
-    Low=3,
-    RedAlert=4,
-
+    High,
+    Medium,
+    Low,
+    RedAlert,
 }
